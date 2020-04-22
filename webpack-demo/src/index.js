10c76197bb2aa59fc49c481a8fcf0935c1033621
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-22 18:06:03 +0900

    Add css loader

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const icon = new Image();
+    icon.src = Icon;
+    icon.height = 100;
+    icon.width = 100;
+
+    element.appendChild(icon);
+
     return element;
 }
 
