fd0cc2333423da295d1e70f282ce9f0f51af46e9
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-23 02:38:57 +0900

    Add font loader

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -4,7 +4,7 @@ import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack', '안녕하세요!!'], ' ');
     element.classList.add('hello');
 
     const icon = new Image();
@@ -13,8 +13,11 @@ function component() {
     icon.width = 100;
 
     element.appendChild(icon);
-
     return element;
 }
 
+const div = document.createElement('div');
+div.innerHTML = '안녕하세요!!';
+
 document.body.appendChild(component());
+document.body.appendChild(div);
