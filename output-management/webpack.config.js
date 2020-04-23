819e6d7b95e6a012e079b719cc13e3546e3a1e60
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-23 23:48:31 +0900

    Add development tools

--- output-management/webpack.config.js
+++ output-management/webpack.config.js
@@ -1,20 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js',
     },
     plugins: [
-        new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Output management',
         }),
     ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
     },
 };
+
+// output-management, development
